40cbd84177c3765d4532feb0ebb9f0e7ecc35a9c
Jose Ivan <jigonzalez930209@gmail.com>
2025-06-22 22:05:05 -0300

    feat: Create `TitleAction` component

---
+++ src/renderer/src/components/vc-analysis/title-action.tsx
@@ -0,0 +1,24 @@
+import { PlusIcon, XIcon } from 'lucide-react'
+import { Button } from '../ui/button'
+import { useVCAnalysis } from './context/use-vc-analysis'
+
+const TitleAction = ({ setOpen }: { setOpen: (open: boolean) => void }) => {
+  const { handleSetGlobalSelectedFiles } = useVCAnalysis()
+  return (
+    <>
+      <Button variant="success" size="icon" onClick={handleSetGlobalSelectedFiles}>
+        <PlusIcon className="w-4 h-4" />
+      </Button>
+      <Button
+        className="absolute right-2"
+        variant="ghost"
+        size="icon"
+        onClick={() => setOpen(false)}
+      >
+        <XIcon className="w-4 h-4" />
+      </Button>
+    </>
+  )
+}
+
+export default TitleAction
